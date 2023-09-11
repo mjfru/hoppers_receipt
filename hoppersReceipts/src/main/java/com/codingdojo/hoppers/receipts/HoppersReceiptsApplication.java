819afb80aae939b9972a10bae8a08b948d51a9af
Matt Frutchey （マット・フルシェー） <108436172+mjfru@users.noreply.github.com>
2023-09-11 06:55:21 -0400

    Add files via upload

---
+++ hoppersReceipts/src/main/java/com/codingdojo/hoppers/receipts/HoppersReceiptsApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.hoppers.receipts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppersReceiptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppersReceiptsApplication.class, args);
+	}
+
+}
---
+++ hoppersReceipts/src/test/java/com/codingdojo/hoppers/receipts/HoppersReceiptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.hoppers.receipts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersReceiptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
